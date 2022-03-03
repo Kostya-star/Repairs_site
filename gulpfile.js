2ab04b367bde18a0c7bdd960b0d01e9d73c2acda
Costya Danilov <konstantindanilov19@hotmail.com>
2022-03-03 14:51:45 +0200

    feat: added gulp-sass into gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,28 @@
-var gulp = require('gulp');
+var {src, dest, watch} = require('gulp');
 var browserSync = require('browser-sync').create();
+// var sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'))
 
-gulp.task ('mytask', function (done) {
-  console.log('Hello world!');
-  done();
-});
+
 
 //static server
-gulp.task('browser-sync', function() {
+function bs() {
+  serveSass();
   browserSync.init({
       server: {
           baseDir: "./"
       }
   });
-  gulp.watch("./*.html").on('change', browserSync.reload);
-});
+  watch("./*.html").on('change', browserSync.reload);
+  watch("./sass/**/*.sass", serveSass);
+  watch("./js/*.js").on('change', browserSync.reload);
+};
+
+function serveSass() {
+  return src("./sass/*.sass")
+      .pipe(sass())
+      .pipe(dest("./css"))
+      .pipe(browserSync.stream());
+};
+
+exports.serve = bs;
